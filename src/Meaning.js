e99dae782b23435f0dbf0a72a31bd6693a752feb
EmmeRox <99152085+EmmeRox@users.noreply.github.com>
2023-02-07 14:41:12 -0600

    new component created

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Synonyms from "./Synonyms";
 
 export default function Meaning(props) {
   return (
@@ -7,9 +8,16 @@ export default function Meaning(props) {
       {props.meaning.definitions.map(function (definition, index) {
         return (
           <div key={index}>
-            <p>{definition.definition}</p>
             <p>
+              <strong>Definition:</strong>
+              <br />
+              {definition.definition}
+            </p>
+            <p>
+              <strong>Example:</strong>
+              <br />
               <em>{definition.example}</em>
+              <Synonyms synonyms={definition.synonyms} />
             </p>
           </div>
         );
