9d662634c6f1319e7b7b10878cb4f94d00f07651
EmmeRox <99152085+EmmeRox@users.noreply.github.com>
2023-02-07 22:12:39 -0600

    Update Synonyms.js

--- src/Synonyms.js
+++ src/Synonyms.js
@@ -3,7 +3,13 @@ import React from "react";
 export default function Synonyms(props) {
   console.log(props);
   if (props.synonyms) {
-    return props.synonyms;
+    return (
+      <div>
+        {props.synonyms.map(function (synonym, index) {
+          return <span key={index}>{synonym}</span>;
+        })}
+      </div>
+    );
   } else {
     return null;
   }
