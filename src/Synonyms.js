c33c0f3a2c4aae0f7666979ec07941e4f8387b79
EmmeRox <99152085+EmmeRox@users.noreply.github.com>
2023-02-10 17:38:54 -0600

    displayed synonyms as list items for clarity

--- src/Synonyms.js
+++ src/Synonyms.js
@@ -4,11 +4,11 @@ export default function Synonyms(props) {
   console.log(props.synonyms);
   if (props.synonyms) {
     return (
-      <div>
+      <ul className="Synonyms">
         {props.synonyms.map(function (synonym, index) {
-          return <span key={index}>{synonym}</span>;
+          return <li key={index}>{synonym}</li>;
         })}
-      </div>
+      </ul>
     );
   } else {
     return null;
